a9538f880c6fececc19a1611a59cc0b4386642e6
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-05-24 15:54:59 +0900

    Table within a card

--- src/components/PagingTable.tsx
+++ src/components/PagingTable.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import { Card, CardMedia, CardTitle } from "material-ui/Card";
+
 interface IPagingTableProps<T> {
 	title: string;
 	thead?: React.ReactNode;
@@ -20,25 +22,51 @@ class PagingTable<T> extends React.Component<IPagingTableProps<T>, IPagingTableS
 	};
 
 	public render() {
-		const {itemsPerPage, page} = this.state;
+		const { itemsPerPage, page } = this.state;
 		const start = itemsPerPage * page;
 		const end = itemsPerPage * (page + 1);
+
+		const styles = this.getStyles();
 		return (
-			<div>
-				<h2>{this.props.title}</h2>
-				{/* TODO: search bar */}
-				<table className="data">
-					{this.props.thead}
-					<tbody>
-						{this.props.items.slice(start, end).map((item, idx) => (
-							<this.props.RowRenderer item={item} key={idx} />
-						))}
-					</tbody>
-					{this.props.tfoot}
-				</table>
-				{/* TODO: pager */}
-			</div>
+			<Card style={styles.card}>
+				<CardTitle
+					actAsExpander
+					showExpandableButton
+					title={this.props.title}
+					style={styles.cardTitle}
+					titleStyle={styles.cardTitleTitle}
+				/>
+				<CardMedia expandable>
+					{/* TODO: search bar */}
+					<table className="data">
+						{this.props.thead}
+						<tbody>
+							{this.props.items.slice(start, end).map((item, idx) => (
+								<this.props.RowRenderer item={item} key={idx} />
+							))}
+						</tbody>
+						{this.props.tfoot}
+					</table>
+					{/* TODO: pager */}
+				</CardMedia>
+			</Card>
 		);
+	}
+
+	private getStyles() {
+		return {
+			card: {
+				margin: 8,
+			} as React.CSSProperties,
+			cardTitle: {
+				padding: "16px 14px 16px 24px",
+			} as React.CSSProperties,
+			cardTitleTitle: {
+				fontSize: 20,
+				lineHeight: "32px",
+				paddingRight: 48,
+			} as React.CSSProperties,
+		};
 	}
 }
 
