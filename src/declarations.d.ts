3ed4e14cc7c978859d2d0ce64d7a1f4f7166c10a
kurgm <gogottt2009-hp@yahoo.co.jp>
2018-01-04 15:47:31 +0900

    Remove unnecessary declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,36 +3,6 @@ declare module "*.md" {
 	export = content;
 }
 
-// Overwrite incorrect definitions
-
-// https://github.com/callemall/material-ui/pull/9125
-declare module "material-ui/utils/withWidth" {
-	import { Breakpoint } from "material-ui/styles/createBreakpoints";
-	export interface WithWidthOptions {
-		resizeInterval: number;
-	}
-
-	export interface WithWidthProps {
-		width: Breakpoint;
-	}
-
-	/**
-	 * By default, returns true if screen width is the same or greater than the given breakpoint.
-	 */
-	export const isWidthUp: (breakpoint: Breakpoint, screenWidth: Breakpoint, inclusive?: boolean) => boolean;
-
-	/**
-	 * By default, returns true if screen width is the same or less than the given breakpoint.
-	 */
-	export const isWidthDown: (breakpoint: Breakpoint, screenWidth: Breakpoint, inclusive?: boolean) => boolean;
-
-	export default function withWidth(
-		options?: WithWidthOptions
-	): <P>(
-			component: React.ComponentType<P & WithWidthProps>
-		) => React.ComponentClass<P & Partial<WithWidthProps>>;
-}
-
 type KageLineData = [number, string];
 
 // tslint:disable-next-line:interface-name
