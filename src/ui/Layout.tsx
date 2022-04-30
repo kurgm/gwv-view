c28524ed5b2c18171f5028b904ec60809b15da02
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2022-04-30 23:46:10 +0900

    Remove custom AppBar

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import { Layout, LayoutProps } from "react-admin";
 import Menu from "./Menu";
-// import AppBar from "./AppBar";
 
 const MyLayout: React.FC<LayoutProps> = (props) => (
 	<Layout
 		{...props}
 		menu={Menu}
-		// appBar={AppBar}
 	/>
 );
 
